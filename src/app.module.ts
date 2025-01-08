60c646de3c41fd2d786b19ac1f9561dc10df79b1
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2025-01-08 21:40:15 +0100

    actualizar Dockerfile, paquetes y version de node

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { join } from 'path';
-import { Module } from '@nestjs/common';
+// import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { HelloWorldModule } from './hello-world/hello-world.module';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { TodoModule } from './todo/todo.module';
+import process from 'process';
 
 
 @Module({
